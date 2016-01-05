0ae7d3d2c9d5db7ef4e2557662225b01e49992a0
Saokz <threebearsatonce@gmail.com>
2016-01-05 01:27:15 -0600

    Update EmailGenerator.java
    Added documentation.

--- EmailGenerator.java
+++ EmailGenerator.java
@@ -14,6 +14,12 @@ public class EmailGenerator {
     private FileWriter writer;
     private ArrayList<Integer> studentIDs;
     
+    /**
+     * Default EmailGenerator constructor
+     * 
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
     public EmailGenerator() throws FileNotFoundException, IOException
     {
     	yearMax = 0; yearMin = 0;
@@ -25,6 +31,16 @@ public class EmailGenerator {
         writer = new FileWriter(file);
     }
     
+    /**
+     * EmailGenerator constructor
+     * 
+     * @param rowMin  The minimum last 3 digits of the studentID
+     * @param rowMax  The maximum last 3 digits of the StudentID
+     * @param yearMin The minimum second digit of the studentID
+     * @param yearMax The maximum second digit of the studentID
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
     public EmailGenerator(int yearMin, int yearMax, int rowMin, int rowMax) throws FileNotFoundException, IOException
     {
     	this.yearMin = yearMin; this.yearMax = yearMax; 
@@ -37,12 +53,21 @@ public class EmailGenerator {
     	writer = new FileWriter(file);
     }
     
+    /**
+     * Generates a six digit studentID code
+     * 
+     * @return A randomly generated student ID that fits the parameters of the Email Generator.
+     */
     public int generateNewID()
     {
     	return MAX_STUDENT_ID_LENGTH + ( yearMin + (int)(Math.random() * ((yearMax - yearMin) + 1)) ) * ((int)Math.pow(10, 4))
                 + ( rowMin + (int)( Math.random() * ( (rowMax - rowMin) + 1 ) ) );
     }
     
+    /**
+     * Generates a list of every possible ID within the parameters of the Email Generator,
+     * and then sorts them for convenience
+     */
     public void generateIDList()
     {
     	System.out.println("Starting...");
@@ -89,22 +114,58 @@ public class EmailGenerator {
         System.out.println("Done.");
     }
     
+    /**
+     * Calculates the maximum amount of studentIDs that can be created
+     * within the parameters of the Email Generator.
+     * 
+     * @return studentID list size
+     */
     public int getPredictedListLength()
     {
     	return (rowMax - rowMin + 1) * (yearMax - yearMin + 1);
     }
     
+    /**
+     * Calculates the maximum amount of studentIDs that can be created
+     * given the parameters specified by the user.
+     * 
+     * @param rowMin  The minimum last 3 digits of the studentID
+     * @param rowMax  The maximum last 3 digits of the StudentID
+     * @param yearMin The minimum second digit of the studentID
+     * @param yearMax The maximum second digit of the studentID
+     * @return studentID list size
+     */
     public int getPredictedListLength(int rowMin, int rowMax, int yearMin, int yearMax)
     {
     	return (rowMax - rowMin + 1) * (yearMax - yearMin + 1);
     }
     
+    /**
+     * Check yearMin, yearMax, rowMin, rowMax, and the difference
+     * between yearMin and yearMax.
+     * 
+     * The mins cannot be greater than the maxes, and the years
+     * cannot be more than 4 years apart.
+     * 
+     * @return true if the parameters of the EmailGenerator have been
+     * 		   set up properly, false otherwise.
+     */
     public boolean checkParameters()
     {
     	return yearMin <= yearMax && rowMin <= rowMax && 
     		   yearMax - yearMin <= MAX_YEAR_DIFFERENCE;
     }
-    
+    /**
+     * Check yearMin, yearMax, rowMin, rowMax, and the difference
+     * between yearMin and yearMax.
+     * 
+     * The mins cannot be greater than the maxes, and the years
+     * cannot be more than 4 years apart. 
+     * 
+     * @param emailInfo {yearMin, yearMax, rowMin, rowMax}
+     * @return true if the parameters of the EmailGenerator have been
+     *         set up properly, false otherwise.
+     */
     public boolean checkParameters(int[] emailInfo)
     {
     	return emailInfo.length == 4 && emailInfo[0] < emailInfo[1] &&
@@ -112,11 +173,22 @@ public class EmailGenerator {
     		   emailInfo[1] - emailInfo[0] <= MAX_YEAR_DIFFERENCE;
     }
     
+    /**
+     * Gets the parameters of the EmailGenerator
+     * 
+     * @return EmailGenerator parameters in array format
+     */
     public int[] getParameters()
     {
     	return new int[] {yearMin, yearMax, rowMin, rowMax};
     }
     
+    /**
+     * Set the parameters of the EmailGenerator
+     * Format: {yearMin, yearMax, rowMin, rowMax}
+     * 
+     * @param emailInfo 
+     */
     public void setParameters(int[] emailInfo)
     {
     	if (!checkParameters(emailInfo) )
@@ -130,11 +202,21 @@ public class EmailGenerator {
     	emailInfo[3] = rowMax;
     }
     
+    /**
+     * Get list of studentIDs
+     * 
+     * @return List of studentIDs
+     */
     public ArrayList<Integer> getStudentIDs()
     {
     	return studentIDs;
     }
     
+    /**
+     * Writes the generated studentIDs to a file
+     * 
+     * @throws IOException
+     */
     public void writeEmailsToFile()throws IOException
     {
     	for (int i: studentIDs)
